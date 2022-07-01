532f5a3f2b8a9c2b1b03cf36534eb1e215af58c7
unknown <alejandrolasadamartinez@gmail.com>
2022-07-01 10:28:50 +0200

    NodeApp

--- server/database.js
+++ server/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const basededatos = process.env.DB_URI;
-//const basededatos = "mongodb://localhost/instaClone";
-//const basededatos = "mongodb+srv://alosamart:Losalosa7@instaclone.30i5o.mongodb.net/instaclone";
+
+
 
 mongoose.connect(basededatos)
 .then(db=>console.log("Base de datos conectada"))
